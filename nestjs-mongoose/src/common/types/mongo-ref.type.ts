a1aaf0a933b8ce41b96a818d6ac8d7a34314b2bd
AlexW <alex.wegener@pflegix.de>
2021-11-24 14:42:07 +0100

    description of type

--- nestjs-mongoose/src/common/types/mongo-ref.type.ts
+++ nestjs-mongoose/src/common/types/mongo-ref.type.ts
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose';
 import { BaseEntity } from '../base-entity.class';
 
+/**
+ * Defines a reference to another mongo db collection.
+ * Will be typed as union of mongo object id and the collection entity type.
+ */
 export type MongoRef<T extends BaseEntity> = T | mongoose.Types.ObjectId;
